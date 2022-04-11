9b346cb332961a1f55a9ff6d9f94ceea6d5711ca
kolathol123 <101928466+kolathol123@users.noreply.github.com>
2022-04-11 13:49:56 +0530

    Update SKSSF-AC.js

--- SKSSF-AC.js
+++ SKSSF-AC.js
@@ -24,7 +24,7 @@ function OnStart()
 	//Add main layout and drawer to app.	
 	app.AddDrawer( drawerScroll, "Left", drawerWidth )
 	
-		
+		check_update();
 }
 
 
@@ -62,7 +62,7 @@ function CreateDrawer()
 	
  web1 = app.CreateWebView( 0.75,1 );
  layDrawer.AddChild(web1);
- web1.LoadUrl("menu.html")
+ web1.LoadUrl("menu.html");
     
     }
 
@@ -70,7 +70,106 @@ function CreateDrawer()
 //Called when hardware menu key pressed.
 function OnMenu( name )
 {  
-   app.OpenDrawer()
+   app.OpenDrawer();
 }
 
 
+
+
+var up;
+function check_update()
+{
+   
+if(app.IsConnected())
+{
+ fetch('https://raw.githubusercontent.com/kolathol123/KolatholeSkssfApp/main/update')
+ .then(response => response.text())
+ .then(data =>{ 
+ up=data;
+ check(up);
+ 
+ 
+ });
+ }
+ 
+ }
+ 
+ function check(data)
+ {
+ var v=app.ReadFile("version.txt");
+ if(Number(up)>Number(v))
+  {
+     alert("update available");
+     app.WriteFile("version.txt",data);
+     
+     const u=["main.html","cluster1.html","menu.html","cluster-name.js","cluster.css","cluster.js","SKSSF-AC.js","style.css"];
+  var mu='https://raw.githubusercontent.com/kolathol123/KolatholeSkssfApp/main/';
+
+  fetch(mu+u[0])
+  .then(response => response.text())
+  .then(data => {
+   app.WriteFile(u[0],data);
+  
+  });
+  
+  fetch(mu+u[1])
+  .then(response => response.text())
+  .then(data => {
+  app.WriteFile(u[1],data);
+  
+  });
+  
+  fetch(mu+u[2])
+  .then(response => response.text())
+  .then(data => {
+  app.WriteFile(u[2],data);
+  
+  });
+  
+  fetch(mu+u[3])
+  .then(response => response.text())
+  .then(data => {
+  app.WriteFile(u[3],data);
+  
+  });
+  
+  fetch(mu+u[4])
+  .then(response => response.text())
+  .then(data => {
+  app.WriteFile(u[4],data);
+  
+  });
+  
+  fetch(mu+u[5])
+  .then(response => response.text())
+  .then(data => {
+  app.WriteFile(u[5],data);
+  
+  });
+  
+  fetch(mu+u[6])
+  .then(response => response.text())
+  .then(data => {
+  app.WriteFile("SKSSF AC.js",data);
+  
+  });
+  
+  fetch(mu+u[7])
+  .then(response => response.text())
+  .then(data => {
+  app.WriteFile(u[7],data);
+  
+  });
+  
+
+
+  /*
+ 
+ var now = new Date().getTime();
+ app.SetAlarm( "Set", 7985, null, now + 2000 );
+ app.Exit( );
+*/
+}
+
+
+ }
